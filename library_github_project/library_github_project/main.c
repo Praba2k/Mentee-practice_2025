a7aff3b339868e18b0e5da37b499a6db543fe523
abubakar <mohammedabubakarsiddiqva12@gmail.com>
2025-04-22 12:17:35 +0530

    created a project file for library managment in microchip

---
+++ library_github_project/library_github_project/main.c
@@ -0,0 +1,18 @@
+/*
+ * library_github_project.c
+ *
+ * Created: 4/22/2025 12:14:13 PM
+ * Author : Mohammed Abubakar VS
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
